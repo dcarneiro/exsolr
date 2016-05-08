dc5a3712fa78edf656a54d87268f6fab29dd7dc3
Daniel Carneiro <daniel.carneiro@nmusic.pt>
2016-05-08 16:12:34 +0100

    add first docs

--- lib/exsolr.ex
+++ lib/exsolr.ex
@@ -1,10 +1,20 @@
 defmodule Exsolr do
+  @moduledoc """
+  Solr wrapper made in Elixir.
+  """
+
   alias Exsolr.Indexer
 
+  @doc """
+  Adds the `document` to Solr.
+  """
   def add(document) do
     Indexer.add(document)
   end
 
+  @doc """
+  Commits the pending changes to Solr
+  """
   def commit do
     Indexer.commit
   end
