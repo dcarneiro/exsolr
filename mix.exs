960c9551ac0c783c634b540188c5a5a530370fde
Aleksandr Podolyanskiy <aleksandr.podolyanskiy@duke.edu>
2018-05-14 09:03:31 -0400

    Updated elixir version to 1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Exsolr.Mixfile do
     [
       app: :exsolr,
       version: "0.0.1",
-      elixir: "~> 1.2",
+      elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description,
