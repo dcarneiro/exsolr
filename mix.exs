ccea4275ffd89ffdb3af14a94fc5922445cb47c5
Aleksandr Podolyanskiy <aleksandr.podolyanskiy@duke.edu>
2018-06-26 12:07:06 -0400

    Change version to 0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exsolr.Mixfile do
   def project do
     [
       app: :exsolr,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
